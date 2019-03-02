1060b7291598eeadd7c1b2e9f0c24205a539fe21
Porchiges <bettencourt.n.f@gmail.com>
2019-03-02 13:44:07 -0500

    more db edits

--- database/db_schema.sql
+++ database/db_schema.sql
@@ -1,35 +1,48 @@
--- ---------------------------
--- Table 'Table1'
--- ---------------------------
+/*Set Database*/
 
-CREATE TABLE 'reported'(
-	'id' INT NOT NULL AUTO_INCREMENT,
-	'w_o_date' DATE NOT NULL,
-	'status_id' VARCHAR NOT NULL,
-	'w_o_number' VARCHAR NOT NULL,
-	'req_number' VARCHAR NOT NULL,
-	'zone_id' VARCHAR NOT NULL,
-	'reporter_id' VARCHAR NOT NULL
+
+USE potholes;
+
+/*CREATE TABLE zone Database*/
+
+
+CREATE TABLE IF NOT EXISTS `zone`(
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`zone_number` VARCHAR(255) NOT NULL,
+	`zone_notes` VARCHAR(255) NOT NULL,
 	PRIMARY KEY (`id`)
+	);
+
+/*CREATE TABLE status Database*/
+
+CREATE TABLE IF NOT EXISTS `status`(
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`name` VARCHAR(255) NOT NULL,
+	PRIMARY KEY (`id`)
+	);
+
+/*CREATE TABLE reporter Database*/
+
+CREATE TABLE IF NOT EXISTS `reporter`(
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`name` VARCHAR(255) NOT NULL,
+	`phone_number` VARCHAR(255) NOT NULL,
+	PRIMARY KEY (`id`)
+	);
+
+/*CREATE TABLE reported Database*/
+
+
+CREATE TABLE IF NOT EXISTS `reported` (
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`status_id` INT NOT NULL,
+	`wo_number` VARCHAR(255) NOT NULL,
+	`req_number` VARCHAR(255) NOT NULL,
+	`zone_id` VARCHAR(255) NOT NULL,
+	`reporter_id` VARCHAR(255) NOT NULL,
+	PRIMARY KEY (`id`),
 	FOREIGN KEY (`status_id`) REFERENCES status(`id`),
 	FOREIGN KEY (`zone_id`) REFERENCES zone(`id`),
-	FOREIGN KEY (`reporter_id`) REFERENCES reporter`id`)
+	FOREIGN KEY (`reporter_id`) REFERENCES reporter(`id`)
 	);
 
-CREATE TABLE 'zone'(
-	'id' INT NOT NULL AUTO_INCREMENT,
-	'zone_number' VARCHAR NOT NULL,
-	'zone_notes' VARCHAR NOT NULL,
-	PRIMARY KEY (`id`)
-	);
-CREATE TABLE 'status'(
-	'id' INT NOT NULL AUTO_INCREMENT,
-	'name' VARCHAR NOT NULL,
-	PRIMARY KEY (`id`)
-	);
-CREATE TABLE 'reporter'(
-	'id' INT NOT NULL AUTO_INCREMENT,
-	'name' VARCHAR NOT NULL,
-	'phone_number' VARCHAR NOT NULL,
-	PRIMARY KEY (`id`)
-	);
