a9ee37666de97e22ad6bba85cab9d179b533bab1
Chas <info@chasmathis.com>
2019-03-02 18:17:46 -0500

    add .htaccess and report walkthrough

--- src/app/report/report.component.ts
+++ src/app/report/report.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, Input, HostListener } from '@angular/core';
+import { MatTableDataSource, MatPaginator, MatSort } from '@angular/material';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { merge } from 'rxjs';
+import { Observable } from 'rxjs/Rx';
+import { map, startWith} from 'rxjs/operators';
+import { Routes, RouterModule, Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material';
 
 @Component({
   selector: 'app-report',
@@ -7,19 +15,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReportComponent implements OnInit {
 
-  constructor() { }
-
     lat:any;
     lon:any;
 
-    ngOnInit(){
+    addressControl = new FormControl('', [Validators.required]);
+    gpsControl = new FormControl('', [Validators.required]);
+
+    isLinear = true;
+    isOptional = false;
+    gpsLocFormGroup: FormGroup;
+    addressLocFormGroup: FormGroup;
+
+    constructor(private _formBuilder: FormBuilder) {}
+
+    getCoords() {
         if(window.navigator.geolocation){
             navigator.geolocation.getCurrentPosition((position) => {
                 this.lat = position.coords.latitude;
                 this.lon = position.coords.longitude;
-                console.log(position.coords.latitude);
-                console.log(position.coords.longitude);
+                this.isOptional = !this.isOptional;
+                this.gpsLocFormGroup.setValue({
+                    gpsLocCtrl: (this.lat+ ", " + this.lon), 
+                });
+                this.gpsLocFormGroup.markAsTouched();
             });
         }
+    }
+
+    ngOnInit() {
+        this.gpsLocFormGroup = this._formBuilder.group({
+            gpsLocCtrl: ['', Validators.required]
+        });
+        this.addressLocFormGroup = this._formBuilder.group({
+            addressLocCtrl: ['', Validators.required]
+        });
     }
 }
