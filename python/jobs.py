f7753d3344ff4d3a924fac92f685cb96cf92a7e8
Zackary Welch <welch.zackary@gmail.com>
2019-03-02 14:07:50 -0500

    More API templates

---
+++ python/jobs.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+from flask import jsonify
+from api_functions import query
+
+def getJobsByLocation(location):
+    pass
+
+def getJobsByStatus(status):
+    pass
+
+def getJobsFromDate(date):
+    pass
+
+def getJobsByTruck(truck):
+    pass
+
+def putJobStatus(id, status):
+    pass
+
+def nextJob(id):
+    pass
---
+++ python/trucks.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+from flask import jsonify
+from api_functions import query
+
+def trucksPut():
+    pass
+
+def getTrucks():
+    pass
+
+def getTrucksByName(name):
+    pass
