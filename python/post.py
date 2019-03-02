00a506eff0e5c381d7a0f5f0434418cb1a1a2474
Zackary Welch <welch.zackary@gmail.com>
2019-03-02 17:48:39 -0500

    Further work on post

--- python/post.py
+++ python/post.py
@@ -3,13 +3,12 @@ from flask import request, jsonify
 from sqlalchemy import create_engine
 from datetime import date, datetime
 from dateutil.relativedelta import relativedelta
-
-import sys
-sys.path.insert(0, './licenselib')
-from licenselib import license
 from api_functions import query
 
 import json
+import sys
+sys.path.insert(0, '../data/places')
+import places
 
 # load configuration
 with open('../config.json') as json_data_file:
@@ -24,4 +23,33 @@ with open('../config.json') as json_data_file:
 engine = create_engine('%s://%s:%s@%s:%s/%s' % (db_conn, db_user, db_pass, db_host, db_port, db_name), pool_pre_ping=True)
 conn = engine.connect()
 
+def readJSON(name, default, json_form):
+    # Read in optional data, or set default values
+    if name in json_form:
+        result = json_form[name]
+    else:
+        result = default
+    return result
 
+def post():
+    json_form = request.get_json(force=True)
+
+    _date = json_form["date"]
+    _organ = json_form["organ"]
+    _location = json_form["location"]
+    _status = json_form["status"]
+    _workorder = json_form["workorder"]
+    _req = json_form["req"]
+    _zone = json_form["zone"]
+    _caller = json_form["caller"]
+    _priority = json_form["caller"]
+
+    order = places.PotholeWorkorder(organ=_organ
+                                    loc_addr=_location
+                                    status=_status
+                                    wo_num=_workorder
+                                    req_num=_req
+                                    zone=_zone
+                                    reporter=_caller
+                                    priority=_priority)
+    #conn.execute(INSERT INTO 'workorders'
