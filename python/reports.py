5ca938f93e2c8e78e22b603d5e410f8dfd99d1a3
Zackary Welch <welch.zackary@gmail.com>
2019-03-02 17:09:52 -0500

    Fixed reports.py for server

--- python/reports.py
+++ python/reports.py
@@ -2,20 +2,20 @@
 from flask import jsonify
 from api_functions import query
 
-def reportPost():
+def post():
     pass
 
-def reportPut():
+def put():
     pass
 
-def getReports():
+def get():
     rv = query("SELECT `id` FROM `reported` ORDER BY `id`")[0]
     if rv:
         return jsonify()
     else:
         return jsonify({'html':'<span>Error: No reports found</span>','text':'Error: No reports found', 'status':404}), 404
 
-def getReportByLocation(location):
+def getLocation(location):
     rv = query("SELECT `id` FROM `reported` WHERE location=%s ORDER BY `id`", location)[0]
     if rv:
         return jsonify()
