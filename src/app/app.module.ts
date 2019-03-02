8a6288f15edbc3b821f23e37c45d414fa31fb2d9
Chas <info@chasmathis.com>
2019-03-02 11:59:08 -0500

    Generate webpage components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ReportComponent } from './report/report.component';
+import { AdminComponent } from './admin/admin.component';
+import { JobsComponent } from './jobs/jobs.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ReportComponent,
+    AdminComponent,
+    JobsComponent
   ],
   imports: [
     BrowserModule
