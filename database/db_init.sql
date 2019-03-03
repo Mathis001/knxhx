6f127520fc2add88da339da07904ffbd8abdf8c9
Zackary Welch <welch.zackary@gmail.com>
2019-03-02 20:44:15 -0500

    Created basic user

--- database/db_init.sql
+++ database/db_init.sql
@@ -7,3 +7,7 @@ INSERT INTO status(`name`) VALUES
 ('Done');
 
 INSERT INTO priority(`name`) VALUES ('Default');
+
+CREATE USER 'knoxpotholes'@'localhost' IDENTIFIED BY 'knoxpotholes';
+GRANT ALL PRIVILEGES ON potholes.* TO 'knoxpotholes'@'localhost' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
