a4792f3e3412237e6be28e07200d0462303356d7
Chas <info@chasmathis.com>
2019-03-02 22:13:04 -0500

    clean api update ui

--- python/api_functions.py
+++ python/api_functions.py
@@ -5,19 +5,6 @@ from sqlalchemy import create_engine
 from dateutil.relativedelta import relativedelta
 import json
 
-# load configuration
-with open('../config.json') as json_data_file:
-    data = json.load(json_data_file)
-    db_conn = data["mysql"]["connector"]
-    db_host = data["mysql"]["hostname"]
-    db_port = data["mysql"]["port"]
-    db_user = data["mysql"]["username"]
-    db_pass = data["mysql"]["password"]
-    db_name = data["mysql"]["database"]
-
-engine = create_engine('%s://%s:%s@%s:%s/%s' % (db_conn, db_user, db_pass, db_host, db_port, db_name), pool_pre_ping=True)
-
-
 # change _create type from string to datetime.date
 def str_to_date(_date):
     if _date:
@@ -43,50 +30,4 @@ def dictzip(row, headers):
     json_data=[]
     for result in row:
         json_data.append(dict(zip(headers,result)))
-    return json_data
-
-def select(selection, json_data):
-    # selection [<json tuple name>, <query>, <formater>, <optional system id>]
-    # adds join table information to json_data object [serials, orders, features, hw/sw contracts, and add_hardware]
-    for select in selection:
-
-        if select[2] == "configTuple":
-            for pair in query(select[1], '')[0]:
-                json_data[select[0]] = [dict(zip(('id','name','active'),pair)) for pair in query(select[1], '')[0]]
-
-        if select[2] == "configTriple":
-            for pair in query(select[1], '')[0]:
-                json_data[select[0]] = [dict(zip(('id','name','hardware_id','active'),pair)) for pair in query(select[1], '')[0]]
-
-        if select[2] == "configList":
-            json_data[select[0]] = [dict(zip(('id','active'),pair)) for pair in query(select[1], '')[0]]
-
-        if select[2] == "systemList":
-            for i in range(len(json_data)):
-                rv = query(select[1], json_data[i]['master_id'])[0]
-                items = []
-                for item in rv:
-                    items += item
-                json_data[i][select[0]] = items
-
-        if select[2] == "serialList":
-            for i in range(len(json_data)):
-                rv = query(select[1], (json_data[i]['master_id'], select[0],))[0]
-                items = []
-                if rv:
-                    for item in rv:
-                        items += item
-                    json_data[i][select[0]] = items
-
-        if select[2] == "orderList":
-            for i in range(len(json_data)):
-                rv = query(select[1], select[3])[0]
-                items = []
-                for item in rv:
-                    items += item
-                json_data[i][select[0]] = items
-
-        if select[2] == "employeeTuple":
-            for pair in query(select[1], '')[0]:
-                json_data[select[0]] = [dict(zip(('id','login','name','tn_auth','department'),pair)) for pair in query(select[1], '')[0]]
     return json_data
--- src/app/jobs/jobs.component.ts
+++ src/app/jobs/jobs.component.ts
@@ -4,57 +4,63 @@ import { NavbarComponent } from '../navbar/navbar.component';
 import { MatTableDataSource, MatPaginator, MatSort, MatIcon, MatSortable, MatSnackBar } from '@angular/material';
 import { merge } from 'rxjs';
 import { Observable } from 'rxjs/Rx';
-
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Routes, RouterModule, Router, ActivatedRoute } from '@angular/router';
 import 'rxjs/add/operator/filter';
 import { switchMap } from 'rxjs/operators';
 
 @Component({
-  selector: 'app-jobs',
-  templateUrl: './jobs.component.html',
-  styleUrls: ['./jobs.component.css']
+	selector: 'app-jobs',
+	templateUrl: './jobs.component.html',
+	styleUrls: ['./jobs.component.css']
 })
 export class JobsComponent implements OnInit {
 
-  constructor(
-        //private apiService: APIService,
-        private router: Router,
-        private route: ActivatedRoute,
-        public snackBar: MatSnackBar
-    ) {  }
+	constructor(
+		//private apiService: APIService,
+		private router: Router,
+		private route: ActivatedRoute,
+		public snackBar: MatSnackBar
+	) {	}
 
-  location:any;
-  address = "617 Cumberland Ave";
-  city = "Knoxville"
-  state = "TN"
-  zip = "37902"
-  lat = "35.9610544"
-  lon = "-83.9206974"
+	location:any;
+	address = "617 Cumberland Ave";
+	city = "Knoxville"
+	state = "TN"
+	zip = "37902"
+	lat = "35.9610544"
+	lon = "-83.9206974"
 
-  url = "https://www.google.com/maps/place/"
-  + this.address.replace(/ /g,"+") + ",+" 
-  + this.city + ",+"
-  + this.state + "+"
-  + this.zip + "/@"
-  + this.lat + ","
-  + this.lon + ",18z"
+	noteControl = new FormControl();
 
-  addressRedirect() {
-  	this.router.navigate(['/externalRedirect', { externalUrl: this.url }]);
-  }
+	url = "https://www.google.com/maps/place/"
+		+ this.address.replace(/ /g,"+") + ",+" 
+		+ this.city + ",+"
+		+ this.state + "+"
+		+ this.zip + "/@"
+		+ this.lat + ","
+		+ this.lon + ",18z"
+
+	addressRedirect() {
+		window.open(this.url, "_blank");
+	}
+
+	post() {
+		
+	}
 
 
-/*    getLocationByID(id) {
-        this.apiService.getLocationByID(id).subscribe((locData) => {
-            this.location = locData;
-        },
-        error => {
-            this.router.navigate(['./404']);
-        });
-    }*/
+/*	getLocationByID(id) {
+		this.apiService.getLocationByID(id).subscribe((locData) => {
+			this.location = locData;
+		},
+		error => {
+			this.router.navigate(['./404']);
+		});
+	}*/
 
-  ngOnInit() {
-  	console.log(this.url);
-  }
+	ngOnInit() {
+		console.log(this.url);
+	}
 
 }
