03dad107b088cf1936364ed5eb39f2964346cd1b
Zackary Welch <welch.zackary@gmail.com>
2019-03-02 19:20:28 -0500

    Added db_init.sql that populates

---
+++ database/db_init.sql
@@ -0,0 +1,4 @@
+INSERT INTO status(name) VALUES ('Under Review') IF EXISTS `status`
+INSERT INTO status(name) VALUES ('In Queue') IF EXISTS `status`
+INSERT INTO status(name) VALUES ('In Progress') IF EXISTS `status`
+INSERT INTO status(name) VALUES ('Done') IF EXISTS `status`
